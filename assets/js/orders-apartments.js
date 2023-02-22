c178b293542eacfc60c305cdc2c8e9064deea7dc
Richard <richardrodriguesgomesdejesus@gmail.com>
2023-02-22 12:05:37 -0300

    corrigindo codigo

--- assets/js/orders-apartments.js
+++ assets/js/orders-apartments.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', (event) =>{
     submitOrder(name,tel,email,msg) 
 })
 function submitOrder(name,tel,email,msg) {
-    fetch('http://apartamentos-aatalogo-imobols.vercel.app/orders-apartments',
+    fetch('https://apartamentos-aatalogo-imobols.vercel.app/orders-apartments',
     {
         method: 'POST',
         headers: {
